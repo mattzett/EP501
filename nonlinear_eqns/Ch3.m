ad17edb4a5dfbbd6a6bd5a35a483ee54d274f9c0
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-09-19 10:00:46 -0400

    added interval halving

--- nonlinear_eqns/Ch3.m
+++ nonlinear_eqns/Ch3.m
@@ -22,7 +22,7 @@ ylabel('f(x)');
 title('Objective function evaluated on grid')
 
 
-%% Illustrate bracketing to find a single root on a given interval
+%% Illustrate inverval halving to find a single root on a given interval
 it=1;
 converged=false;    %make sure we enter iterations
 a0=pi/4;
@@ -72,9 +72,65 @@ end %while
 if (it==maxit)
     warning('Max number of iterations used...')
 end %if
-disp('Root value through bracketing:  ');
+disp('Root value through inverval halving:  ');
 disp(xnew);
 disp('Number of iterations required to reach tolerance:  ');
 disp(it-1);
 
+
+%% Illustrate false position to find a single root on a given interval
+it=1;
+converged=false;    %make sure we enter iterations
+a=a0;
+b=b0;
+while (~converged && it<=maxit)
+    it=it+1;
+    
+    c=a-f(a)/((f(b)-f(a))/(b-a));
+    
+    aprev=a;           %save these for plotting
+    bprev=b;
+    if(f(a)*f(c)<0)    %c will be our new endpoint
+        b=c;
+        left=true;
+    else               %a will be our new staring point
+        a=c;
+        left=false;
+    end %if
+    
+    xnew=c;
+    fval=f(xnew);
+    converged=abs(fval)<tol;
+        
+    if (verbose)       %plot progress of the algorithm toward convergence
+        figure(2);
+        clf;
+        hold on;
+        plot(x,ygrid);
+        ax=axis;
+        axis([a0,b0,ax(3:4)]);
+        if (left)
+            plot(aprev,0,'k^','MarkerSize',10,'LineWidth',2);
+            plot(bprev,0,'r^','MarkerSize',10,'LineWidth',2);
+        else
+            plot(aprev,0,'r^','MarkerSize',10,'LineWidth',2);
+            plot(bprev,0,'k^','MarkerSize',10,'LineWidth',2);
+        end %if
+        plot(c,0,'ko','MarkerSize',10,'LineWidth',2);
+        y=(f(bprev)-f(aprev))/(bprev-aprev)*(x-aprev)+f(aprev);
+        plot(x,y,'--');
+        hold off;
+        xlabel('x');
+        ylabel('f(x)');
+        title(sprintf('x = %f',xnew))
+        pause;
+    end %if
+end %while
+if (it==maxit)
+    warning('Max number of iterations used...')
+end %if
+disp('Root value through false position:  ');
+disp(xnew);
+disp('Number of iterations required to reach tolerance:  ');
+disp(it-1);
 
