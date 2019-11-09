09a6db6f08cd458ddfebec76ec759c8959084919
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-11-09 16:30:54 -0500

    more ODEs

--- ODEs/Ch7.m
+++ ODEs/Ch7.m
@@ -10,7 +10,7 @@ dt=t(2)-t(1);
 y0=1;
 alpha=2;
 ybar=y0*exp(-alpha*t);
-figure;
+figure(1);
 plot(t,ybar,'o-');
 xlabel('t');
 ylabel('y(t)');
@@ -20,8 +20,8 @@ set(gca,'FontSize',20);
 %% Forward Euler solution
 yfwd=zeros(1,N);
 yfwd(1)=y0;
-for it=2:N
-    yfwd(it)=yfwd(it-1)*(1-alpha*dt);
+for n=2:N
+    yfwd(n)=yfwd(n-1)*(1-alpha*dt);
 end %for
 hold on;
 plot(t,yfwd,'--');
@@ -30,11 +30,10 @@ plot(t,yfwd,'--');
 %% Backward Euler solution
 ybwd=zeros(1,N);
 ybwd(1)=y0;
-for it=2:N
-    ybwd(it)=ybwd(it-1)/(1+alpha*dt);
+for n=2:N
+    ybwd(n)=ybwd(n-1)/(1+alpha*dt);
 end %for
 plot(t,ybwd,'-.');
-legend('exact','fwd Eul','bwd Eul.')
 
 
 %% Illustration of stability
@@ -43,15 +42,15 @@ dtmargin=2/alpha2;
 dtstable=1/10*dtmargin;
 dtunstable=1.01*dtmargin;
 
-dt=dtunstable;
-t2=0:dt:5;
+dt2=dtunstable;
+t2=0:dt2:5;
 N=numel(t);
 y2bar=y0*exp(-alpha2*t2);
 
 y2fwd=zeros(1,N);
 y2fwd(1)=y0;
-for it=2:N
-    y2fwd(it)=y2fwd(it-1)*(1-alpha2*dt);
+for n=2:N
+    y2fwd(n)=y2fwd(n-1)*(1-alpha2*dt2);
 end %for
 
 figure;
@@ -66,16 +65,79 @@ plot(t2,y2fwd,'--');
 %% Illustration of convergence
 %run the code above with finer mesh, etc.
 
+
 %% Second order method; RK2
+yRK2=zeros(1,N);
+yRK2(1)=y0;
+for n=2:N
+    yhalf=yRK2(n-1)+dt/2*(-alpha*yRK2(n-1));
+    yRK2(n)=yRK2(n-1)+dt*(-alpha*yhalf);
+end %for
+figure(1);
+plot(t,yRK2,':')
 
 
 %% RK2 stability considerations, FDE analysis
+adt=linspace(0.01,3,20);
+ladt=numel(adt);
+G=zeros(ladt,1);
+for igain=1:ladt
+    G(igain)=(1-adt(igain)+1/2*adt(igain).^2);
+end %for
+figure;
+plot(adt,G,'o')
+set(gca,'FontSize',20);
+xlabel('\alpha \Delta t');
+ylabel('gain factor');
 
 
 %% RK4 example; comparison against first and second order methods
+yRK4=zeros(1,N);
+yRK4(1)=y0;
+for n=2:N
+    dy1=dt*fRK(t(n-1),yRK4(n-1),alpha);
+    dy2=dt*fRK(t(n-1)+dt/2,yRK4(n-1)+dy1/2,alpha);
+    dy3=dt*fRK(t(n-1)+dt/2,yRK4(n-1)+dy2/2,alpha);
+    dy4=dt*fRK(t(n-1)+dt,yRK4(n-1)+dy3,alpha);
+    
+    yRK4(n)=yRK4(n-1)+1/6*(dy1+2*dy2+2*dy3+dy4);
+end %for
+figure(1);
+plot(t,yRK4,'^-')
+legend('exact','fwd Eul','bwd Eul.','RK2','RK4')
 
 
 %% RK2 and systems of equations, oscillating charge example
+q=-1.6e-19;
+m=9.1e-31;
+B=10000e-9;
+omega=q*B/m;
+tmin=0;
+tmax=2*2*pi/abs(omega);
+t=linspace(tmin,tmax,20);
+dt=t(2)-t(1);
+lt=numel(t);
+
+vx=zeros(1,lt);
+vy=zeros(1,lt);
+vy(1)=1e3;
+vx(1)=1e3;
+for n=2:lt
+    vxhalf=vx(n-1)+dt/2*(omega*vy(n-1));
+    vyhalf=vy(n-1)-dt/2*(omega*vx(n-1));
+    
+    vx(n)=vx(n-1)+dt*(omega*vyhalf);
+    vy(n)=vy(n-1)-dt*(omega*vxhalf);    
+end %for
+figure;
+plotyy(t,vx,t,vy);
+
+x=cumtrapz(t,vx);
+y=cumtrapz(t,vy);
+vz=1e3;
+z=vz*t;
+figure;
+comet3(x,y,z)
 
 
 %% Handling multiple time scales (ODE stiffness, book examples)
