05c827cffa1770a52a082c21ad62c9976d930d71
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-11-19 10:48:11 -0500

    started hyperbolic script

--- PDEs/Ch10_parabolic.m
+++ PDEs/Ch10_parabolic.m
@@ -3,15 +3,14 @@ addpath ../linear_algebra;
 
 
 %% Define a 1D space and time grid in x,t for a test problem
-lx=25;
-ly=25;
-N=lx*ly;
+lx=64;
 a=1;     %here a,b are the endpoints of the x-domain
 b=a;     %use a square region for a test problem
 x=linspace(a,b,lx);
 dx=x(2)-x(1);        %grid spacing
 dt=0.1;              %time step
-
+tmin=0;
+tmax=5;
 
 %% Parameters of the parabolic equation
 lambda=2;
@@ -20,13 +19,19 @@ lambda=2;
 %% FTCS implementation
 
 
-%% Trapezoidal implementation
+%% Trapezoidal implementation, note matrix solutions are more efficiently handled thru tri-diagonal solver...
 
 
 %% Compare two solutions
 
 
 %% Compute and plot the analytical solution (see https://github.com/gemini3d/GEMINI-docs/blob/master/test_descriptions/GEMINItests.pdf for a derivation)
+
+
+%% Illustrate stability with FTCS
+
+
+%% Adaptive time stepping?
 
 
 %% Reset paths when we are done (for consistency, cleanliness)
---
+++ PDEs/Ch11_hyperbolic.m
@@ -0,0 +1,20 @@
+%% Define a 1D space and time grid in x,t for a test problem
+lx=64;
+N=lx*ly;
+a=1;     %here a,b are the endpoints of the x-domain
+b=a;     %use a square region for a test problem
+x=linspace(a,b,lx);
+dx=x(2)-x(1);        %grid spacing
+dt=0.1;              %time step
+
+
+%% Lax method
+
+
+%% Lax-Wendroff
+
+
+%% Upwinding, Godunov's method
+
+
+
