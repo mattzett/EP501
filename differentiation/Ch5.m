4d134ce18b9b4177988626eaee4e13866d3c4e34
Matthew Zettergren <mattzett@users.noreply.github.com>
2019-11-21 07:24:44 -0500

    added some comments to Ch5.m

--- differentiation/Ch5.m
+++ differentiation/Ch5.m
@@ -66,32 +66,36 @@ contourf(x,y,f);
 dx=x(2)-x(1);
 dy=y(2)-y(1);
 
+
+%% Gradient
 gradx=zeros(size(f));
 grady=zeros(size(f));
 
-%x component
+%x component of gradient
 for ix=2:lx-1
-    gradx(:,ix)=(f(:,ix+1)-f(:,ix-1))/2/dx;
+    gradx(:,ix)=(f(:,ix+1)-f(:,ix-1))/2/dx;    %\partial/\partial x
 end %for
 gradx(:,1)=(f(:,2)-f(:,1))/dx;
 gradx(:,lx)=(f(:,lx)-f(:,lx-1))/dx;
 
-%y component
+%y component of gradient
 for iy=2:ly-1
-    grady(iy,:)=(f(iy+1,:)-f(iy-1,:))/2/dy;
+    grady(iy,:)=(f(iy+1,:)-f(iy-1,:))/2/dy;    %\partial/\partial y
 end %for
 grady(1,:)=(f(2,:)-f(1,:))/dy;
 grady(ly,:)=(f(ly,:)-f(ly-1,:))/dy;
 
-%add quiver on top
+%add quiver on top of color plot
 hold on;
 quiver(X,Y,gradx,grady,'Color','white','LineWidth',2);
 set(gca,'FontSize',24);
 
+
+%% Take the Laplacian by taking divergence of the previously computed gradient
 f=gradx;
 g=grady;
 
-%x component
+%x derivative part of the divergence
 divx=zeros(size(f));
 for ix=2:lx-1
     divx(:,ix)=(f(:,ix+1)-f(:,ix-1))/2/dx;
@@ -99,7 +103,7 @@ end %for
 divx(:,1)=(f(:,2)-f(:,1))/dx;
 divx(:,lx)=(f(:,lx)-f(:,lx-1))/dx;
 
-%y component
+%y derivative part of the divergence
 divy=zeros(size(y));
 for iy=2:ly-1
     divy(iy,:)=(g(iy+1,:)-g(iy-1,:))/2/dy;
@@ -107,7 +111,7 @@ end %for
 divy(1,:)=(g(2,:)-g(1,:))/dy;
 divy(ly,:)=(g(ly,:)-g(ly-1,:))/dy;
 
-div=divx+divy;
+div=divx+divy;    %this is really laplacian b/c input is gradient
 
 figure;
 surface(x,y,div);
